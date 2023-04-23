6df0027fcd65b12ad1b663c53ff91361c47a659e
DaBoyce <121953358+DaBoyce@users.noreply.github.com>
2023-04-23 14:05:39 -0400

    Update apartmentScraper.py

--- apartmentScraper.py
+++ apartmentScraper.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
+import os
 
-urlToScrape = "https://www.apartments.com/apartments/under-1900-pet-friendly-cat/air-conditioning-washer-dryer-dishwasher-walk-in-closets/?sk=08c73016dc636d78d762a89107215994&bb=g34xux3r6H0p4qwx-B&sfmin=600&so=2&mid=20230630"
-priceMaximum = 1800
-squareFootageMinimum = 700
+priceMaximum = 1900
+squareFootageMinimum = 400
 
 def extract_unit_data(unit_container):
     # Extract apartment price
-    apartment_price = unit_container.find('div', class_='pricingColumn').find('span', class_=None).text.strip()
-    
+    apartment_price = unit_container.find('div', class_='pricingColumn').find('span', class_="").text.strip()
+
     try:
         apartment_price = int(apartment_price.replace('$', '').replace(',', ''))
     except ValueError:
@@ -35,6 +35,8 @@ def extract_unit_data(unit_container):
 
     return apartment_price, square_footage, available_from
 
+
+
 def extract_property_name(soup):
     property_name_element = soup.find('div', class_='propertyName')
     if property_name_element:
@@ -43,6 +45,7 @@ def extract_property_name(soup):
     else:
         return "Not found"
 
+
 def scrape_url(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
@@ -50,6 +53,7 @@ def scrape_url(url):
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive"
     }
+
 
     try:
         response = requests.get(url, headers=headers, timeout=60)
@@ -63,7 +67,7 @@ def scrape_url(url):
 
     property_name = extract_property_name(soup)
     
-    unit_containers = soup.find_all('li', class_='unitContainer')
+    unit_containers = soup.find_all('div', class_='grid-container js-unitExtension')
 
     for unit_container in unit_containers:
         apartment_price, square_footage, available_from = extract_unit_data(unit_container)
@@ -88,8 +92,8 @@ def extract_apartment_urls(url):
     try:
         response = requests.get(url, headers=headers, timeout=60)
     except requests.exceptions.RequestException as e:
-        print(f"RequestException occurred for {url}. Skipping...\nError: {e}")
-        return
+                print(f"RequestException occurred for {url}. Skipping...\nError: {e}")
+                return
 
     soup = BeautifulSoup(response.text, "html.parser")
     url_array = []
@@ -104,7 +108,7 @@ def extract_apartment_urls(url):
 
 ############################ MAIN #################################
 
-url = urlToScrape
+url = "https://www.apartments.com/apartments/under-1900-pet-friendly-cat/air-conditioning-washer-dryer/?sk=81cd9227fa151a155ac5e6500d2a0eda&bb=l0ught9p4H_0vvq8P&so=2&mid=20230701"
 url_array = extract_apartment_urls(url)
 
 all_unit_data = []
@@ -117,14 +121,30 @@ for apartment in url_array:
             if data['Apartment Price'] <= priceMaximum and data['Square Footage'] >= squareFootageMinimum:
                 all_unit_data.append(data)
 
-df = pd.DataFrame(all_unit_data)
+new_df = pd.DataFrame(all_unit_data)
 output_filename = "apartment_data.xlsx"
 
-# Remove the file if it already exists
-import os
+# Check if the file exists
 if os.path.isfile(output_filename):
-    os.remove(output_filename)
+    # Read the existing file
+    existing_df = pd.read_excel(output_filename)
 
-# Save the data to the Excel file
-df.to_excel(output_filename, index=False)
+    # Append new data to the existing DataFrame only if there is no duplicate
+    for _, row in new_df.iterrows():
+        duplicate_entry = existing_df[
+            (existing_df['Property Name'] == row['Property Name']) &
+            (existing_df['Apartment Price'] == row['Apartment Price']) &
+            (existing_df['Square Footage'] == row['Square Footage'])
+        ]
+
+        if duplicate_entry.empty:
+            existing_df = existing_df.append(row, ignore_index=True)
+
+    combined_df = existing_df
+else:
+    # If the file doesn't exist, create a new DataFrame
+    combined_df = new_df
+
+# Save the combined data to the Excel file
+combined_df.to_excel(output_filename, index=False)
 
